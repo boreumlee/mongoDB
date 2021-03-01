05e55dcfb66d6ae8e509ff46ebdf94562b7f113b
Luna <boreum4151@gmail.com>
2021-03-02 00:40:56 +0900

    mongoose 기본 연결

---
+++ index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
