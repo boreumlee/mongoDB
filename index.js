48ade0d3b49724ce25b4ffb27021fa532205b98b
Luna <boreum4151@gmail.com>
2021-03-07 16:44:19 +0900

    find-mongodb

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose
+const connected_mongo = mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("MongoDB Connected..."))
+  .then(() => console.log("MongoDB Connected... 되었음"))
   .catch((err) => console.log(err));
+
+module.exports = { connected_mongo };
